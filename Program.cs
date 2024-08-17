84d13229a511566d2ceaceb4e1da74b37671156f
Drake Elwartowski <squaduck@gmail.com>
2024-08-16 22:58:07 -0400

    Attempt 5 - Use Longs where possible I got a really high result but this should be correct?

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿/* Attempt 4
-Ran in 00:04:31.9084680
+﻿/* Attempt 5
+Ran in 00:01:16.4777112
 
-real    4m35.743s
-user    35m10.645s
-sys     0m1.246s
+real    1m20.391s
+user    9m57.077s
+sys     0m0.476s
 */
 
 namespace Graveler;
@@ -30,8 +30,75 @@ class Program
         Parallel.For<RandAndByte>(0, NUM_ROUNDS_TO_SIM, () => new(), (x, loopState, threadLocal) =>
         {
             byte NumberOf1sRolled = 0;
+            long l = 0;
 
-            for (int i = 0; i < 231; i++)
+            for (int i = 0; i < 231 / 31; i++)
+            {
+                l = threadLocal.r.NextInt64(); // 63 random bits. each dice roll is 2 bits. 31 dice rolls per long 
+                if ((l & 0b11) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 2)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 4)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 6)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 8)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 10)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 12)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 14)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 16)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 18)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 20)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 22)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 24)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 26)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 28)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 30)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 32)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 34)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 36)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 38)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 40)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 42)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 44)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 46)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 48)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 50)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 52)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 54)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 56)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 58)) == 0)
+                    NumberOf1sRolled++;
+                if ((l & (0b11 << 60)) == 0)
+                    NumberOf1sRolled++;
+            }
+            for (int i = 0; i < 231 % 31; i++)
             {
                 if (threadLocal.r.Next(4) == 0)
                     NumberOf1sRolled++;
