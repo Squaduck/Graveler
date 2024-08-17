33c9c9936fc033cad8c18662b6ab262b1d0abe4a
Drake Elwartowski <squaduck@gmail.com>
2024-08-16 23:55:45 -0400

    Attempt 5.5 - Replace longs with ints Proper handling of longs is a little bit slower than ints. I should have tried this first.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿/* Attempt 5
-Ran in 00:01:16.4777112
+﻿/* Attempt 5.5
+Ran in 00:01:22.7753993
 
-real    1m20.391s
-user    9m57.077s
-sys     0m0.476s
+real    1m24.636s
+user    10m50.635s
+sys     0m0.625s
 */
 
 namespace Graveler;
@@ -30,75 +30,43 @@ class Program
         Parallel.For<RandAndByte>(0, NUM_ROUNDS_TO_SIM, () => new(), (x, loopState, threadLocal) =>
         {
             byte NumberOf1sRolled = 0;
-            long l = 0;
+            int l = 0;
 
-            for (int i = 0; i < 231 / 31; i++)
+            for (int i = 0; i < 231 / 15; i++)
             {
-                l = threadLocal.r.NextInt64(); // 63 random bits. each dice roll is 2 bits. 31 dice rolls per long 
-                if ((l & 0b11L) == 0)
+                l = threadLocal.r.Next(); // 31 random bits. each dice roll is 2 bits. 15 dice rolls per long 
+                if ((l & 0b11) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 2)) == 0)
+                if ((l & (0b11 << 2)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 4)) == 0)
+                if ((l & (0b11 << 4)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 6)) == 0)
+                if ((l & (0b11 << 6)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 8)) == 0)
+                if ((l & (0b11 << 8)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 10)) == 0)
+                if ((l & (0b11 << 10)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 12)) == 0)
+                if ((l & (0b11 << 12)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 14)) == 0)
+                if ((l & (0b11 << 14)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 16)) == 0)
+                if ((l & (0b11 << 16)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 18)) == 0)
+                if ((l & (0b11 << 18)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 20)) == 0)
+                if ((l & (0b11 << 20)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 22)) == 0)
+                if ((l & (0b11 << 22)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 24)) == 0)
+                if ((l & (0b11 << 24)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 26)) == 0)
+                if ((l & (0b11 << 26)) == 0)
                     NumberOf1sRolled++;
-                if ((l & (0b11L << 28)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 30)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 32)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 34)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 36)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 38)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 40)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 42)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 44)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 46)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 48)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 50)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 52)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 54)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 56)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 58)) == 0)
-                    NumberOf1sRolled++;
-                if ((l & (0b11L << 60)) == 0)
+                if ((l & (0b11 << 28)) == 0)
                     NumberOf1sRolled++;
             }
-            for (int i = 0; i < 231 % 31; i++)
+            for (int i = 0; i < 231 % 15; i++)
             {
                 if (threadLocal.r.Next(4) == 0)
                     NumberOf1sRolled++;
