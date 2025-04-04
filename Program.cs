897b12f1847687ef9dfee393768230ccd6c1c582
Drake Elwartowski <squaduck@gmail.com>
2025-04-03 22:45:53 -0400

    Revise method, now only within margin of error of original

--- Program.cs
+++ Program.cs
@@ -49,27 +49,22 @@ class Program
             
             231 bits / 8 bits per byte = 28.875 bytes per vector. 
             58 bytes total
+            256-231=25
             */
 
-            Span<byte> randBytes = stackalloc byte[58];
+            byte[] randBytes = new byte[64];
             threadLocal.r.NextBytes(randBytes);
 
-            Vector256<byte> a = Vector256.Create(randBytes[0], randBytes[1], randBytes[2], randBytes[3], randBytes[4], randBytes[5], randBytes[6], randBytes[7],
-                                                 randBytes[8], randBytes[9], randBytes[10], randBytes[11], randBytes[12], randBytes[13], randBytes[14], randBytes[15],
-                                                 randBytes[16], randBytes[17], randBytes[18], randBytes[19], randBytes[20], randBytes[21], randBytes[22], randBytes[23],
-                                                 randBytes[24], randBytes[25], randBytes[26], randBytes[27], (byte)(randBytes[28] & 0b01111111), 0, 0, 0);
+            Vector256<byte> a = Vector256.Create(randBytes);
 
-            Vector256<byte> b = Vector256.Create(randBytes[29], randBytes[30], randBytes[31], randBytes[32], randBytes[33], randBytes[34], randBytes[35], randBytes[36],
-                                                 randBytes[37], randBytes[38], randBytes[39], randBytes[40], randBytes[41], randBytes[42], randBytes[43], randBytes[44],
-                                                 randBytes[45], randBytes[46], randBytes[47], randBytes[48], randBytes[49], randBytes[50], randBytes[51], randBytes[52],
-                                                 randBytes[53], randBytes[54], randBytes[55], randBytes[56], (byte)(randBytes[57] & 0b01111111), 0, 0, 0);
+            Vector256<byte> b = Vector256.Create(randBytes, 32);
 
             Vector256<long> c = Vector256.BitwiseAnd(a, b).AsInt64();
 
             NumberOf1sRolled = (byte)BitOperations.PopCount((ulong)c[0]);
             NumberOf1sRolled += (byte)BitOperations.PopCount((ulong)c[1]);
             NumberOf1sRolled += (byte)BitOperations.PopCount((ulong)c[2]);
-            NumberOf1sRolled += (byte)BitOperations.PopCount((ulong)c[3]);
+            NumberOf1sRolled += (byte)BitOperations.PopCount((ulong)c[3] >>> 25); // Discard 25 bits.
 
             threadLocal.b = NumberOf1sRolled > threadLocal.b ? NumberOf1sRolled : threadLocal.b;
             return threadLocal;
