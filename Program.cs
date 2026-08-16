c3ac736cf451cdcffea961b0db8030a7ae8091d8
Drake Elwartowski <squaduck@gmail.com>
2026-08-16 16:38:14 -0400

    Use Parallel.Invoke to run a tighter loop on each thread with less overhead

--- Program.cs
+++ Program.cs
@@ -33,59 +33,79 @@ class Program
         System.Threading.Lock toBeLocked = new(); // For thread safety.
         const int NUM_ROUNDS_TO_SIM = 1000000000;
 
-        Parallel.For<RandAndByte>(0, NUM_ROUNDS_TO_SIM, () => new(), (x, loopState, threadLocal) =>
+        int threads = Environment.ProcessorCount; // This ignores big-little designs, or any other kind of non-uniform CPU topology
+
+        int leftoverCount = NUM_ROUNDS_TO_SIM % threads;
+        int baseCount = NUM_ROUNDS_TO_SIM / threads;
+
+        Action[] actions = new Action[threads];
+
+        for (int action_idx = 0; action_idx < threads; action_idx++)
         {
-            byte NumberOf1sRolled;
+            int num_todo = baseCount;
 
-            /*
-            Rethinking it yet again: 
-            We want to simulate 231 1-in-4 probabilities. 
-            Each 1-in-4 probability is equivalent to 2 1-in-2 probabilities ANDed together.
-            If we can get 462 1-in-2 probabilities, and AND pairs together, we'll have 231 1-in-4 probabilities
-            Each bit in a random number is a 1-in-2 probability
-            So we need 462 bits in two 231-bit vectors to AND together.
-            Sounds like AVX512 could work!
-            Or avx256 would suffice, since my 7600x doesn't have full width avx-512. 
-            It only has a half-width avx512 pipeline, and does two* operations to complete avx512 instructions.
-                *Two times what it would have to do for a 256-wide pipeline. It's probably more than one "step."
-            
-            231 bits / 8 bits per byte = 28.875 bytes per vector. 
-            58 bytes total
-            256-231=25
-            */
+            if (action_idx < leftoverCount)
+                num_todo++;
 
-            // Get random bytes
-            Span<byte> randBytes = threadLocal.arr; // Cast array to span. Faster than stackalloc-ing every time. Casting to a span also seems to be faster than just using the array directly? (Also, keeping a Memory<byte> in the thread local was slower too.)
-            threadLocal.r.NextBytes(randBytes); // Fill a slice excluding 3 bytes? we're going to discard 25 bits anyway, so we could just not generate 24 of them.
-
-            // Create a Vector256 out of the first 32 bytes
-            Vector256<byte> a = Vector256.Create(randBytes);
-
-            // Create a Vector256 out of the second 32 bytes
-            Vector256<byte> b = Vector256.Create(randBytes[32..64]);
-
-            // AND them together and reinterpret as int64s for less PopCount calls.
-            Vector256<ulong> c = Vector256.BitwiseAnd(a, b).AsUInt64();
-
-            // Get PopCount. (No avx512_vpopcntdq in dotnet yet, see https://github.com/dotnet/runtime/issues/96162)
-            NumberOf1sRolled = unchecked((byte)BitOperations.PopCount(c[0]));
-            NumberOf1sRolled += unchecked((byte)BitOperations.PopCount(c[1]));
-            NumberOf1sRolled += unchecked((byte)BitOperations.PopCount(c[2]));
-            NumberOf1sRolled += unchecked((byte)BitOperations.PopCount(c[3] >>> 25)); // Discard 25 bits.
-
-            // Parallel reduction stuff. If this go round was our best, save it.
-            if (NumberOf1sRolled > threadLocal.b)
-                threadLocal.b = NumberOf1sRolled;
-                
-            return threadLocal;
-        }, (x) =>
-        {
-            lock (toBeLocked)
+            actions[action_idx] = () =>
             {
-                if (x.b > HighestNumberOf1sRolled)
-                    HighestNumberOf1sRolled = x.b;
-            }
-        });
+                Span<byte> randBytes = stackalloc byte[64];
+                Random random = new();
+                byte localHighestNumberOf1sRolled = 0;
+
+                for (int i = 0; i < num_todo; i++)
+                {
+                    /*
+                        Rethinking it yet again: 
+                        We want to simulate 231 1-in-4 probabilities. 
+                        Each 1-in-4 probability is equivalent to 2 1-in-2 probabilities ANDed together.
+                        If we can get 462 1-in-2 probabilities, and AND pairs together, we'll have 231 1-in-4 probabilities
+                        Each bit in a random number is a 1-in-2 probability
+                        So we need 462 bits in two 231-bit vectors to AND together.
+                        Sounds like AVX512 could work!
+                        Or avx256 would suffice, since my 7600x doesn't have full width avx-512. 
+                        It only has a half-width avx512 pipeline, and does two* operations to complete avx512 instructions.
+                            *Two times what it would have to do for a 256-wide pipeline. It's probably more than one "step."
+
+                        231 bits / 8 bits per byte = 28.875 bytes per vector. 
+                        58 bytes total
+                        256-231=25
+                    */
+
+                    // Get random bytes
+                    random.NextBytes(randBytes);
+
+                    // Create a Vector256 out of the first 32 bytes
+                    Vector256<byte> a = Vector256.Create(randBytes);
+
+                    // Create a Vector256 out of the second 32 bytes
+                    Vector256<byte> b = Vector256.Create(randBytes[32..64]);
+
+                    // AND them together and reinterpret as int64s for less PopCount calls.
+                    Vector256<ulong> c = Vector256.BitwiseAnd(a, b).AsUInt64();
+
+                    byte NumberOf1sRolled;
+
+                    // Get PopCount. (No avx512_vpopcntdq in dotnet yet, see https://github.com/dotnet/runtime/issues/96162)
+                    NumberOf1sRolled = unchecked((byte)BitOperations.PopCount(c[0]));
+                    NumberOf1sRolled += unchecked((byte)BitOperations.PopCount(c[1]));
+                    NumberOf1sRolled += unchecked((byte)BitOperations.PopCount(c[2]));
+                    NumberOf1sRolled += unchecked((byte)BitOperations.PopCount(c[3] >>> 25)); // Discard 25 bits.
+
+                    // Parallel reduction stuff. If this go round was our best, save it.
+                    if (NumberOf1sRolled > localHighestNumberOf1sRolled)
+                        localHighestNumberOf1sRolled = NumberOf1sRolled;
+                }
+
+                lock (toBeLocked)
+                {
+                    if (localHighestNumberOf1sRolled > HighestNumberOf1sRolled)
+                        HighestNumberOf1sRolled = localHighestNumberOf1sRolled;
+                }
+            };
+        }
+
+        Parallel.Invoke(actions);
 
         sw.Stop();
 
